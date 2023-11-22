edeb9c78a70218b6a105840b89456eab8b748923
jxturbo <jxturbo@gmail.com>
2023-11-22 16:47:51 +0800

    update

--- GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/TransformMesh.cs
+++ GMAPS_Oct_2023_Worksheets STUDENT/Assets/02_MATRICES_worksheet/TransformMesh.cs
@@ -22,7 +22,7 @@ public class TransformMesh : MonoBehaviour
    {
         meshManager = GetComponent<MeshManager>();
         pos = new HVector2D(gameObject.transform.position.x, gameObject.transform.position.y);
-        //Translate(1,1);
+        Translate(1,1);
         Rotate(-45);
    }
 
